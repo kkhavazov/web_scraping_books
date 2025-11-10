59ea6b384f17b5e7555602258806c44f436a7012
kkhavazov <kkhavazov@gmail.com>
2025-11-10 21:05:42 +0100

    DataFrame with all information available
    Created a structured DataFrame with books names, authors, formats and prices

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import numpy as np
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-links = []
+names_books = []
+authors = []
+formats = []
+prices = []
+
 for page in range(1,10):
     page_request = requests.get(f'https://www.wonderbk.com/shop/books/fiction?page={page}')
     soup = BeautifulSoup(page_request.text, "html.parser")
     tag = soup.find_all('article')
     
-    for l in tag:
-        links.append(l.find_all('a')[0]['href'])
-print(len(links))
+    for book in tag:
+        names_books.append(book.find("h3"))
+        authors.append(book.find_all("p")[0].string)
+        formats.append(book.find_all("p")[0].string)
+        prices.append(book.find_all("p")[0].string)
+
+books = pd.DataFrame({'Author': authors, 'Formats': formats, 'Price': prices}, index = names_books)
+print(books.shape)
